ed6e78518710953845cb467812a3872346f8776a
nilberthsouza <nilberth@protonmail.com>
2019-12-02 20:12:59 -0300

    compilando e funcionando, corrigido erros de digitação

--- main.go
+++ main.go
@@ -19,32 +19,20 @@ var posts []Post
 // a sintaxe para criar um endpoint é assim:
 //router.HandleFunc("/<sua-url>",<nome-da-função>).Methods("<method>")
 
-func main(){
-    router := mux.NewRouter()
-    
-    router.HandleFunc("/posts", getPosts).Methods("GET")
-    router.HanfleFunc("/posts", createPost).Methods("POST")
-    router.HandleFunc("/posts/{id}",getPost).Methods("GET")
-    router.HandleFunc("/posts/{id}", updatePost).Methods("PUT")
-    router.HandleFunc("/post/{id}", deletePost).Methods("DELETE")
-
-    http.ListenAndServe(":8000",router)
-
-}
 
 
-func getPost(w http.Response.Writer, r *http.Request){
+func getPosts(w http.ResponseWriter, r *http.Request){
     w.Header().Set("Content-Type","application/json")
     json.NewEncoder(w).Encode(posts)
 
 }
 
-func getPosts(w http.Response.Writer, r *http.Request){
-    w.Heade().Set("Content-Type","application/json")
+func getPost(w http.ResponseWriter, r *http.Request){
+    w.Header().Set("Content-Type","application/json")
     params := mux.Vars(r)
     for _, item := range posts{
         if item.ID == params["id"]{
-            json.NewEnconder(w).Encode(item)
+            json.NewEncoder(w).Encode(item)
             break
         }
         return
@@ -58,7 +46,7 @@ func createPost(w http.ResponseWriter, r *http.Request){
     _ = json.NewDecoder(r.Body).Decode(post)
     post.ID = strconv.Itoa(rand.Intn(1000000))
     posts = append(posts, post)
-    json.NewEncoder(w).Enconde(&post)
+    json.NewEncoder(w).Encode(&post)
 
 }
 
@@ -73,23 +61,45 @@ func updatePost(w http.ResponseWriter, r *http.Request){
             _ = json.NewDecoder(r.Body).Decode(post)
             post.ID = params["id"]
             posts = append(posts, post)
-            json.NewEnconder(w).Encode(&post)
+            json.NewEncoder(w).Encode(&post)
 
             return
             
         
         }
     }
-    json.NewEncoder(w).Encode(post)
+    json.NewEncoder(w).Encode(posts)
 }
 
 func deletePost(w http.ResponseWriter, r * http.Request){
     w.Header().Set("Content-Type", "application/json")
     params := mux.Vars(r)
-    for _, item := range posts{
+    for index, item := range posts{
         if item.ID == params["id"]{
-            posts= append(posts[:index], posts[index+1])
+            posts = append(posts[:index], posts[index+1:]...)
+            break
         }
     }
-    json.NewEncoder(w).Encode(books)
+    json.NewEncoder(w).Encode(posts)
+}
+
+
+func main(){
+    router := mux.NewRouter()
+    //apenas para fim de testes
+    posts = append(posts, Post{ID: "1",Title:"My first post",Body:
+    "this is the content of my first post"})
+    posts = append(posts, Post{ID:"2",Title: "My second post",Body:
+    "This is the content of my second post"})
+
+
+
+    router.HandleFunc("/posts", getPosts).Methods("GET")
+    router.HandleFunc("/posts", createPost).Methods("POST")
+    router.HandleFunc("/posts/{id}",getPost).Methods("GET")
+    router.HandleFunc("/posts/{id}", updatePost).Methods("PUT")
+    router.HandleFunc("/post/{id}", deletePost).Methods("DELETE")
+
+    http.ListenAndServe(":8000",router)
+
 }
